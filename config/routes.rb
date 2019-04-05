ac13971aa78eae15fa80d46b3a38157a9ad1dc84
Oblum1989 <oscar.blum@gmail.com>
2019-04-05 15:41:46 -0500

    se aplican estilos y falta arreglar navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  
+  root 'products#index'  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get '/products', to: 'products#index'
   # get '/products/new', to: 'products#new', as: 'new_product'
@@ -7,8 +9,12 @@ Rails.application.routes.draw do
   # get '/products/:id/edit', to: 'products#edit', as: 'edit_product'
   # patch '/products/:id', to: 'products#update'
   # delete '/products/:id', to: 'products#destroy'
-  resources :products
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
-  root 'products#index'
+  resources :products
+  resources :users, only: [:new, :create]
+  
 
 end
